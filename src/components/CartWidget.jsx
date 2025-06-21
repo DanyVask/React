ea44389db8c0678210aff401af13c01889763f3a
DanyVask <danielavasquezrios@gmail.com>
2025-06-21 11:31:56 -0500

    ajustes en el carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,10 +4,10 @@ import carrito from "../assets/carrito.png";
 const CartWidget = () => {
     return (
         <div className="cartwidget">
-            <img className="logo-cartwidget" src="{carrito}" alt="" />
+            <img className="logo-cartwidget" src={carrito} alt="" />
             <h2>6</h2>
         </div>
     )
 }
 
-export default CartWidget
+export default CartWidget;
